a5f056ae0fd73af30dbffcb4dfebce1e3f4d1148
DarkoMarko97 <125009439+DarkoMarko97@users.noreply.github.com>
2025-07-17 12:06:41 +0200

    Add files via upload
    Sto seguendo il corso: 100 Days of Code: The Complete Python Pro Bootcamp dal sito Udemy.
    Il primo progetto è un generatore di nomi di Band

---
+++ band_name_generator.py
@@ -0,0 +1,4 @@
+print("Benvenuto in questo generatore di Band!")
+city=str(input("In che città sei nato? \n"))
+pet=str(input("Come si chiama il tuo animale domestico? \n"))
+print("Il nome della tua Band sarà: " + pet + " " + city)
