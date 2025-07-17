052a5de9b78f49e918836c865e64f60a2bb69440
DarkoMarko97 <125009439+DarkoMarko97@users.noreply.github.com>
2025-07-17 13:58:22 +0200

    Add files via upload
    Un semplicissimo calcolatore di mance

---
+++ tip_calculator.py
@@ -0,0 +1,14 @@
+print(len(str(12345)))
+print(len("12345"))
+numero="12345"
+print(type(len(str(12345))))
+print(type(numero))
+floats = 5.5
+kevin = True
+true_numero = 12345
+print(type(floats))
+print(type(kevin))
+print(type(numero) , type(floats) , type(kevin) , type(true_numero))
+print(int(floats) , str(kevin) , bool(true_numero), float(numero))
+
+print("Number of letters in your name: " + str(len(input("Enter your name "))))
